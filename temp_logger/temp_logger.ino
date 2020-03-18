906dd21fe015543314c41eae9c996e863f3c27e1
Nam Ngo <nam.ngo.shop@gmail.com>
2020-03-17 20:37:33 -0700

    connecting to ada io

--- temp_logger/temp_logger.ino
+++ temp_logger/temp_logger.ino
@@ -18,11 +18,22 @@
 #include <Adafruit_MQTT.h>
 
 #include <DHT.h>
+#include <string>
+#include <Preferences.h>
+#include "SPIFFS.h"
 
 #define DHTPIN 23
 #define DHTTYPE DHT22
 
 DHT dht_(DHTPIN, DHTTYPE);
+
+Preferences preferences;
+
+std::unique_ptr<AdafruitIO_WiFi> io; //(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+
+AdafruitIO_Feed *temp_c_feed; // = io.feed("counter");
+AdafruitIO_Feed *temp_f_feed;
+AdafruitIO_Feed *humidity_feed;
 
 float readHumidity()
 {
@@ -44,10 +55,60 @@ float readTemperature(bool fahrenheit = true)
   return temp;
 }
 
+String readFile(String filename)
+{
+  if (!SPIFFS.begin(true))
+  {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return "";
+  }
+  auto file = SPIFFS.open(filename);
+  if (!file)
+  {
+    Serial.println("Failed to open file for reading");
+    return "";
+  }
+  return file.readString();
+}
+
+void setupSecret()
+{
+  // uncomment to setup secrets
+  // do not check in secrets!
+  // preferences.putString("IO_USERNAME", "");
+  // preferences.putString("IO_KEY", "");
+  // preferences.putString("WIFI_SSID", "");
+  // preferences.putString("WIFI_PASS", "");
+}
+
 void setup()
 {
   Serial.begin(115200);
   dht_.begin();
+  auto secret = readFile("/secret.txt");
+  Serial.println(secret);
+  preferences.begin("temp_logger", false);
+  setupSecret();
+
+  auto username = preferences.getString("IO_USERNAME").c_str();
+  auto key = preferences.getString("IO_KEY").c_str();
+  auto wifi_ssid = preferences.getString("WIFI_SSID").c_str();
+  auto wifi_pass = preferences.getString("WIFI_PASS").c_str();
+
+  //AdafruitIO_WiFi io1(username, key, wifi_ssid, wifi_pass);
+  io = std::make_unique<AdafruitIO_WiFi>(username, key, wifi_ssid, wifi_pass);
+  temp_c_feed = io->feed("temp_c");
+  temp_f_feed = io->feed("temp_c");
+  humidity_feed = io->feed("temp_c");
+
+  while (io->status() < AIO_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println();
+  Serial.println(io->statusText());
 }
 
 void loop()
